d3f825bac04993a06cfb8ef6fcdc85e0f4fbd0ec
DanielAlmeida309 <58252911+DanielAlmeida309@users.noreply.github.com>
2021-12-01 10:51:10 +0000

    Create Files from Front-End

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const PORT = process.env.PORT || 8000;
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 require("./rotas/rotas")(app);
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+app.use(express.static('public'));
